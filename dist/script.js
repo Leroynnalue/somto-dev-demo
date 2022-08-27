b9dfd62ce79ed4aa9e29b908ffcd714d5d26f19d
ALIENROY <leroynnalue65@gmail.com>
2022-08-28 00:57:59 +0100

    pages added

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,29 @@ app.controller('Quotes',($scope,$http) => {
         $scope.quoteInfo = `"Whatever the mind of man can conceive and believe, it can achieve."`
         $scope.quoteAuthor = `Napoleon Hill`
 })
+
+// Custom Cursor
+var cusor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cusor.style.left = x + 'px';
+  cusor.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cusor.classList.add('cusorhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cusor.classList.remove('cusorhover')
+});
